dbe0339d7863a3bd6573209e41b228415a54b404
VitaliiBobylov <bobylevvitalyanatolievich@gmail.com>
2026-02-08 20:06:18 +0200

    arshi

--- src/pages/contact/contact.page.ts
+++ src/pages/contact/contact.page.ts
@@ -1,13 +1,16 @@
-export function contactPage(): string {
-  return `
-      <section class="section">
-        <div class="container">
-          <h2>Контакти</h2>
-          <p>Напишіть нам, і ми з вами звʼяжемося</p>
-          <a class="button" id="contactBtn">Написати</a>
-        </div>
-      </section>
-    `;
+export function contactPage(): HTMLElement {
+  const section = document.createElement("section");
+  section.className = "section";
+
+  section.innerHTML = `
+    <div class="container">
+      <h2>Контакти</h2>
+      <p>Напишіть нам, і ми з вами звʼяжемося</p>
+      <a class="button" id="contactBtn">Написати</a>
+    </div>
+  `;
+
+  return section;
 }
 
 export function initContactEvents(): void {
